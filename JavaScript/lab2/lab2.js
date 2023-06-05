f3ea11828c3a92a34cc37921890b641b392443e1
vivarasymizmy <105988618+vivarasymizmy@users.noreply.github.com>
2023-06-05 13:20:22 +0300

    asd

--- JavaScript/lab2/lab2.js
+++ JavaScript/lab2/lab2.js
@@ -1,58 +1,76 @@
+/**
+ * Возводит х в степень n 
+ * @param {number} x основание степени
+ * @param {number} n степень
+ * @returns x  в степени n
+ */
 function pow(x, n) {
     return x ** n;
 }
-
+/**
+ * Возвращает сумму от 1 до n
+ * @param {number} n максимальное число
+ * @returns сумму от 1 до n
+ */
 function sumTo(n) {
-   let sum = 0;
-   for (let i = 1; i <= n; i++) {
-       sum += i
-   }
-   return sum;
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i
+    }
+    return sum;
 }
 
-   
-function factorial(n) {   
-   if (n<=1) return 1n;
-   else {
-       res = BigInt (n) * BigInt(factorial(n-1))
-       return res;
-   }  
+/**
+ * Считает факториал числа n
+ * @param {number} n основание факториала
+ * @returns факториал  n
+ */
+function factorial(n) {
+    if (n <= 1) return 1n;
+    else {
+        return BigInt(n) * factorial(n - 1);
+    }
 }
- function fib(n){
-   a = BigInt(1); 
-   b=BigInt(1); 
-   if (n== BigInt(0))
-   {
-       return BigInt(0); 
-   }
-   else
-   { 
-       for (i = BigInt(3); i<=n; i++) 
-       {
-           c = a+b;
-           a=b;
-           b=c;
-       }
-       return b;
-   }
+/**
+ * n-ый член ряда Фибоначчи
+ * @param {number} n 
+ * @returns  n-ый член ряда Фибоначчи
+ */
+function fib(n) {
+    a = BigInt(1);
+    b = BigInt(1);
+    if (n == BigInt(0)) {
+        return BigInt(0);
+    }
+    else {
+        for (i = BigInt(3); i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
 }
-
-function compare(x)
-{
-return function(y)
-{
-   if (y>x) return true;
-   else if (y<x) return false;
-   else if (y==x) return null;
+/**
+ * Возвращает анонимную булеву функцию результата сравнения числа с x
+ * @param {number} x число для сравнения
+ * @returns возвращает анонимную булеву функцию результата сравнения числа с x
+ */
+function compare(x) {
+    return function (y) {
+        if (y > x) return true;
+        else if (y < x) return false;
+        else if (y == x) return null;
+    }
 }
-}
-
-function sum()
-{
-   var result=0; 
-   for (var i=0; i<arguments.length; i++)
-   {
-       result+=arguments[i] 
-   }
-   return result;
+/**
+ * Сумма всех принятых аргументов функцией 
+ * @returns сумму всех принятых аргументов функцией 
+ */
+function sum() {
+    var result = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        result += arguments[i]
+    }
+    return result;
 }
