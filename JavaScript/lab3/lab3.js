8ccd6896c44c9e9e985d768b50de9e4e45f0471b
vivarasymizmy <105988618+vivarasymizmy@users.noreply.github.com>
2023-06-05 13:34:32 +0300

    asd

--- JavaScript/lab3/lab3.js
+++ JavaScript/lab3/lab3.js
@@ -1,6 +1,11 @@
 import {
     fib
 } from './lab2.js';
+/**
+ * Программа возвращает дробную часть числа
+ * @param {number} num 
+ * @returns дробную часть числа
+ */
 export function getDecimal(num) {
     if (Math.trunc(num) == num) {
         return 0;
@@ -10,12 +15,16 @@ export function getDecimal(num) {
     }
 
     if (num > 1) {
-        return +(num - Math.trunc(num)).toFixed(2);
+        return +(num - Math.trunc(num)).toFixed(10);
     }
 }
 
 
-
+/**
+ * Функция  возвращает строку str с заглавным первым символом
+ * @param {string} str 
+ * @returns  возвращает строку str с заглавным первым символом
+ */
 export function ucFirst(str) {
     if (!str) {
         return str;
@@ -26,14 +35,23 @@ export function ucFirst(str) {
 }
 
 
-
+/**
+ * Функция возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false
+ * @param {string} str 
+ * @returns возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false
+ */
 export function checkSpam(str) {
     let newstr = str.toLowerCase();
     return newstr.includes('viagra') || newstr.includes('xxx');
 }
 
 
-
+/**
+ * Функция проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на символ многоточие "…"
+ * @param {string} str 
+ * @param {number} maxlength 
+ * @returns отформатированная строка 
+ */
 export function truncate(str, maxlength) {
     if (str.length < maxlength) {
         return str;
@@ -44,7 +62,11 @@ export function truncate(str, maxlength) {
 }
 
 
-
+/**
+ *  Функция преобразует строку вида 'var-test-text' в 'varTestText'
+ * @param {string} str 
+ * @returns строка вида'varTestText'
+ */
 export function camelize(str) {
     let str_copy = str.split("-");
     let strnew = "";
@@ -54,6 +76,11 @@ export function camelize(str) {
     }
     return strnew;
 }
+/**
+ * Функция  возвращает массив, заполненный числами Фибоначчи до n-го (не включая его)
+ * @param {number} n 
+ * @returns  массив, заполненный числами Фибоначчи до n-го (не включая его)
+ */
 export function fibs(n) {
     let mas = [];
     for (let i = 0; i < n; i += 1) {
@@ -61,12 +88,22 @@ export function fibs(n) {
     }
     return mas;
 }
+/**
+ * Функция возвращает массив из тех же элементов, но отсортированный по убыванию
+ * @param {array} arr 
+ * @returns  массив из тех же элементов, но отсортированный по убыванию
+ */
 export function arrReverseSorted(arr) {
     let arr_copy = arr;
     return arr_copy.sort(function (a, b) {
         return b - a;
     })
 }
+/**
+ * Функция массив уникальных, не повторяющихся значений массива arr
+ * @param {array} arr 
+ * @returns массив уникальных, не повторяющихся значений массива arr
+ */
 export function unique(arr) {
     return Array.from(new Set(arr));
 }
